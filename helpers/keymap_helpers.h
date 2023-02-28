08d4e36efc8451815d9b5823d356c92f7acc7286
spork <feedthefoon@gmail.com>
2023-02-28 00:59:43 -0800

    cleaning up macros

---
+++ helpers/keymap_helpers.h
@@ -0,0 +1,56 @@
+// -------- BEHAVIORS ---------
+#define ZMK_BEHAVIOR(name, cell) \
+    name: name {\
+        label = #name;\
+        #binding-cells = <cell>;
+
+#define SHIFT_MORPH(name, bind1, bind2) \
+    ZMK_BEHAVIOR(name, 0)\
+        compatible = "zmk,behavior-mod-morph";\
+        bindings = <&kp bind1>, <&kp bind2>;\
+        mods = <(MOD_LSFT|MOD_RSFT)>;\
+    }
+
+#define STICKY(name, bind) \
+    ZMK_BEHAVIOR(name, 1)\
+        compatible = "zmk,behavior-sticky-key";\
+        bindings = <bind>;\
+        release-after-ms = <STICKY_TIME_MS>;\
+        quick-release;\
+    }
+
+#define HOLDTAP_(name, bind1, bind2, tapterm, quickterm) \
+    ZMK_BEHAVIOR(name, 2)\
+        compatible = "zmk,behavior-hold-tap";\
+        bindings = <bind1>, <bind2>;\
+        tapping-term-ms = <tapterm>;\
+        quick-tap-ms = <quickterm>;\
+
+#define HOLDTAP_TAP_PREFFERED(name, bind, tapterm, quickterm) \
+    HOLDTAP_(name, bind, &kp, tapterm, quickterm) \
+        flavor = "tap-preferred" ;\
+        global-quick-tap;\
+    }
+
+#define MODTAP_TAP_BALANCED_POSITIONAL(name, tapterm, quickterm, holdkeys) \
+    HOLDTAP_(name, &kp, &kp, tapterm, quickterm) \
+        flavor = "balanced";\
+        hold-trigger-key-positions = <holdkeys>;\
+    }
+
+
+// -------- COMBOS ---------
+#define COMBO(name, keypress, keypos, term) \
+combo_##name {\
+    timeout-ms = <term>;\
+    bindings = <keypress>;\
+    key-positions = <keypos>;\
+}
+
+// -------- CONDITIONAL_LAYERS ---------
+#define TRI_LAYER(name, ifs, thens) \
+    tri_layer_##name {\
+        if-layers = <ifs>;\
+        then-layer = <thens>;\
+    }
+
